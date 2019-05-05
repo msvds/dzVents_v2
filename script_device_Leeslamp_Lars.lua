e848009a78c027eb1223cc3eb4e00de4160f3970
msvds <martijnvanderschoot@gmail.com>
2019-05-05 20:01:10 +0200

    Update script_device_Leeslamp_Lars.lua

--- script_device_Leeslamp_Lars.lua
+++ script_device_Leeslamp_Lars.lua
@@ -6,7 +6,7 @@ return {
 	},
 
 	execute = function(domoticz, device)
-		if (domoticz.time.matchesRule('at 7:10-20:50') or domoticz.time.matchesRule('at 23:00-06:00')) then
+		if (domoticz.time.matchesRule('at 7:10-21:00') or domoticz.time.matchesRule('at 23:00-06:00')) then
 		
 		else
 			domoticz.devices('Leeslamp Lars').switchOff().silent()
