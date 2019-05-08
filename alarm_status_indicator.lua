a0ef2570dcd3224f11c109b7c3bdf259778e65bb
msvds <martijnvanderschoot@gmail.com>
2019-05-08 08:42:13 +0200

    Update alarm_status_indicator.lua

--- alarm_status_indicator.lua
+++ alarm_status_indicator.lua
@@ -14,8 +14,8 @@ return {
 			if device.state == domoticz.SECURITY_DISARMED then				
 				domoticz.devices('Gateway light eetkamer').switchSelector(0)
 				domoticz.devices('Gateway light hal boven').switchSelector(0)
-				domoticz.devices('Xiaomi RGB Gateway eetkamer').switchOff() -- light off
-				domoticz.devices('Xiaomi RGB Gateway hal boven').switchOff() -- light off
+				domoticz.devices('Gateway light eetkamer').switchOff() -- light off
+				domoticz.devices('Gateway light hal boven').switchOff() -- light off
 				domoticz.log('SECURITY DISARMED',domoticz.LOG_INFO)
 			elseif device.state == domoticz.domoticz.SECURITY_ARMEDAWAY then 
 				domoticz.devices('Gateway light eetkamer').switchSelector(10)
